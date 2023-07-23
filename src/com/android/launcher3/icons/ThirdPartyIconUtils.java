5bebbd4d4af16cdc7224e3e5cef0c12098d6b17d
minaripenguin <minaripenguin@users.noreply.github.com>
2023-07-23 15:01:57 +0800

    Ortus Launcher: Integrate themed icons picker to Launcher3
    Change-Id: I666cfd9c6164f85109643ea012e7a4ee525fdfbd
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>
    Signed-off-by: ifloraxd <ifloraxd@gmail.com>

--- src/com/android/launcher3/icons/ThirdPartyIconUtils.java
+++ src/com/android/launcher3/icons/ThirdPartyIconUtils.java
@@ -10,6 +10,7 @@ import com.android.launcher3.icons.clock.CustomClock;
 import com.android.launcher3.icons.clock.DynamicClock;
 import com.android.launcher3.icons.pack.IconPackManager;
 import com.android.launcher3.icons.pack.IconResolver;
+import com.android.launcher3.util.Themes;
 
 class ThirdPartyIconUtils {
     static Drawable getByKey(Context context, ComponentKey key, int iconDpi,
@@ -24,12 +25,14 @@ class ThirdPartyIconUtils {
             return CustomClock.getClock(context, icon, resolver.clockData());
         }
 
-        // Google Clock goes second, but only if the icon pack does not override it.
-        if (icon == null && key.componentName.equals(DynamicClock.DESK_CLOCK)) {
-            String packageName = key.componentName.getPackageName();
-            IconProvider.ThemeData td = IconProvider.INSTANCE.get(context)
-                    .getThemeDataForPackage(packageName);
-            return ClockDrawableWrapper.forPackage(context, packageName, iconDpi, td);
+            // Google Clock goes second, but only if the icon pack does not override it.
+            if (icon == null && key.componentName.equals(DynamicClock.DESK_CLOCK)) {
+                String packageName = key.componentName.getPackageName();
+                String themedIconPack = Themes.getThemedIconPack(context);
+                IconProvider.ThemeData td = IconProvider.INSTANCE.get(context)
+                        .getThemeDataForPackage(packageName, themedIconPack);
+                return ClockDrawableWrapper.forPackage(context, packageName, iconDpi, td);
+            }
         }
 
         // Google Calendar is checked last. Only applied if the icon pack does not override it.
